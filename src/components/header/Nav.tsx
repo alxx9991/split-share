d70ab54d8ed0ac0e199fd58e73d508270db38d0c
Alex Lin <alex.lin9991@gmail.com>
2022-05-13 16:36:03 +1000

    cleared unused imports

--- src/components/header/Nav.tsx
+++ src/components/header/Nav.tsx
@@ -1,4 +1,4 @@
-import { Link, useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Button from "../ui/Button";
 import classes from "./Nav.module.css";
 import { useLocation } from "react-router-dom";
